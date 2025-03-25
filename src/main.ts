239cd4a647d3634186ec7b1447a8c12a39795f65
pdamianik <39028343+pdamianik@users.noreply.github.com>
2025-03-25 16:58:42 +0100

    Add basic action
    Signed-off-by: pdamianik <39028343+pdamianik@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import * as github from '@actions/github'
 
 /**
  * The main function for the action.
@@ -8,18 +8,29 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const token: string = core.getInput('token')
+    const label: string = core.getInput('label')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const octokit = github.getOctokit(token)
+    const context = github.context
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const prs = await octokit.rest.pulls.list({
+      ...context.repo,
+      state: 'open'
+    })
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    for (const pr of prs.data) {
+      if (
+        pr.author_association === 'OWNER' &&
+        pr.labels.find(({ name }) => name == label) !== undefined
+      ) {
+        octokit.rest.pulls.merge({
+          ...context.repo,
+          pull_number: pr.number,
+          merge_method: 'squash'
+        })
+      }
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
